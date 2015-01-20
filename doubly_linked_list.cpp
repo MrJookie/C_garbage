6f0ad68d8f9296f6ee0d3511a2e80c85e1a5b4ff
MrJookie <black.lemon1@gmail.com>
2015-01-20 18:27:49 +0100

    Bi-directional linked list

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -1,26 +1,59 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-struct osoba {
-	int cislo;
-	osoba *dalsi;
+struct node {
+	char *name;
+	node *next;
+	node *prev;
 };
+
+node *allocate_node(void)
+{
+	node *new_node = (struct node *)malloc(sizeof(node));
+	if (!new_node)
+		exit(1);
+
+	return new_node;
+}
+
+void add_node(node *end, node *new_node, char *name)
+{
+	new_node->name = name;
+	new_node->next = NULL;
+	new_node->prev = end;
+	end->next = new_node;
+}
+
+node *find_end(node *ptr)
+{
+	node *iter = ptr;
+
+	while (iter->next != NULL)
+		iter = iter->next;
+
+	return iter;
+}
 
 int main(int argc, char *argv[])
 {
-	osoba *zakaznici = NULL;
-	osoba *os;
+	node *nodes = NULL;
+	node *first;
+	node *iter;
+
+	//Allocate first node (head)
+	first = (struct node *)malloc(sizeof(node));
+	first->name = "Head";
+	first->next = NULL;
+	first->prev = NULL;
+	nodes = first;
 
 	for (int i = 0; i < 10; i++) {
-		osoba *novy_zakaznik;
-		novy_zakaznik = (struct osoba *)malloc(sizeof(osoba));
-		novy_zakaznik->cislo = i;
-		novy_zakaznik->dalsi = zakaznici;
-		zakaznici = novy_zakaznik;
+		add_node(find_end(nodes), allocate_node(), "Lucas");
 	}
 
-	for (os = zakaznici; os; os = os->dalsi) {
-		printf("cislo: %d\n", os->cislo);
+	for (iter = nodes; iter; iter = iter->next) {
+		printf("node: %s\n", iter->name);
 	}
 
 	getchar();
