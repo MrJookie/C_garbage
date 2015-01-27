8d144ad0b44e070ec0dc0ed52c69b6b40fa55bab
MrJookie <black.lemon1>
2015-01-27 01:26:21 +0100

    qsort strings or integers

--- qsort_strings.cpp
+++ qsort_strings.cpp
@@ -6,25 +6,25 @@ char *values[] = { "avocado", "apple", "ananas", "orange", "banana", "kiwi" };
 
 int cmpints(const void *a, const void *b)
 {
-	return ( *(int*)a - *(int*)b );
+	return (*(int*)a - *(int*)b);
 }
 
 int cmpstrs(const void *a, const void *b)
 {
-	return strcmp( *(const char**)a, *(const char**)b );
+	return strcmp(*(const char**)a, *(const char**)b);
 }
 
 int main(int argc, char *argv[])
 {
 	printf("Before sorting the list is:\n");
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 6; i++) {
 		printf("%s ", values[i]);
 	}
 
-	qsort(values, 5, 4, cmpstrs);
+	qsort(values, 6, sizeof(char *), cmpstrs);
 
 	printf("\n\nAfter sorting the list is:\n");
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 6; i++) {
 		printf("%s ", values[i]);
 	}
 
